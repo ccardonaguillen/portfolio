28f8ff8120e730b23b86522022178187514cb9fc
ccardona <ccardonaguillen@gmail.com>
2023-02-06 17:37:57 +0100

    Style Contact homepage section

--- src/layouts/Home/Contact/index.js
+++ src/layouts/Home/Contact/index.js
@@ -3,34 +3,59 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 import './style.css';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 
 export default function Contact() {
     return (
         <section id="home-contact">
-            <p>You can find me on</p>
-            <a href={'https://github.com/ccardonaguillen/'}>
-                <FontAwesomeIcon icon={faGithub} id="github-logo" alt="Github" />
-            </a>
-            <a href={'https://www.linkedin.com/in/ccardonaguillen/'}>
-                <FontAwesomeIcon icon={faLinkedin} id="github-logo" alt="Github" />
-            </a>
-            <p>
-                Or you can leave a message below and I will get back to go you as soon as possible
-            </p>
+            <h3 className="section-heading">Find me on</h3>
+            <div id="contact-links">
+                <a href={'https://github.com/ccardonaguillen/'}>
+                    <FontAwesomeIcon icon={faGithub} id="github-logo" alt="Github" />
+                </a>
+                <a href={'https://www.linkedin.com/in/ccardonaguillen/'}>
+                    <FontAwesomeIcon icon={faLinkedin} id="github-logo" alt="Github" />
+                </a>
+            </div>
+            <p>Or you leave a message below and I will get back to go you as soon as possible</p>
             <form id="contact-message" onSubmit={() => false}>
                 <div>
-                    <label htmlFor="name">Name</label>
-                    <input type="text" id="name" name="name" required={true}></input>
+                    {/* <label htmlFor="name">Name</label> */}
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        placeholder="Name"
+                        required={true}
+                        autoComplete="off"
+                    ></input>
                 </div>
                 <div>
-                    <label htmlFor="email">Email adress</label>
-                    <input type="email" id="email" name="email" required={true}></input>
+                    {/* <label htmlFor="email">Email adress</label> */}
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="Email address"
+                        required={true}
+                        autoComplete="off"
+                    ></input>
                 </div>
                 <div>
-                    <label htmlFor="message">Your message</label>
-                    <textarea id="message" name="message" required={true}></textarea>
+                    {/* <label htmlFor="message">Your message</label> */}
+                    <textarea
+                        id="message"
+                        name="message"
+                        rows="5"
+                        placeholder="Your message"
+                        required={true}
+                        autoComplete="off"
+                    ></textarea>
                 </div>
-                <button type="submit">Send</button>
+                <button type="submit" id="send-message">
+                    <p>Send</p>
+                    <FontAwesomeIcon icon={faEnvelope} />
+                </button>
             </form>
         </section>
     );
