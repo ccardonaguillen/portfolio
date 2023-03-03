8cffd3e81bd4b64216bba2dd3303410acab66be6
ccardona <ccardonaguillen@gmail.com>
2023-03-03 10:34:08 +0100

    Add more items and icons to devItems

--- src/assets/devicons/index.js
+++ src/assets/devicons/index.js
@@ -1,4 +1,3 @@
-
 function importAll(r) {
     let images = {};
     r.keys().forEach((img) => {
@@ -9,21 +8,23 @@ function importAll(r) {
 
 const devIcons = importAll(require.context('../../assets/devicons', false, /\.(png|jpe?g|svg)$/));
 
-const devItems = [
-    { name: 'python', title: 'Python', icon: devIcons.python },
-    { name: 'js', title: 'Javascript', icon: devIcons.javascript },
-    { name: 'html', title: 'HTML5', icon: devIcons.html5 },
-    { name: 'css', title: 'CSS3', icon: devIcons.css3 },
-    { name: 'react', title: 'React', icon: devIcons.react },
-    { name: 'node', title: 'Node.JS', icon: devIcons.nodejs },
-    { name: 'express', title: 'Express', icon: devIcons.express },
-    { name: 'mongodb', title: 'MongoDB', icon: devIcons.mongodb },
-    { name: 'jest', title: 'Jest', icon: devIcons.jest },
-    { name: 'webpack', title: 'Webpack', icon: devIcons.webpack },
-    { name: 'npm', title: 'NPM', icon: devIcons.npm },
-    { name: 'django', title: 'Django', icon: devIcons.django },
-    { name: 'numpy', title: 'Numpy', icon: devIcons.numpy },
-    { name: 'pandas', title: 'Pandas', icon: devIcons.pandas },
-];
+const devItems = {
+    python: { title: 'Python', icon: devIcons.python },
+    js: { name: 'js', title: 'Javascript', icon: devIcons.javascript },
+    html: { name: 'html', title: 'HTML5', icon: devIcons.html5 },
+    css: { name: 'css', title: 'CSS3', icon: devIcons.css3 },
+    sass: { name: 'sass', title: 'SASS', icon: devIcons.sass },
+    react: { name: 'react', title: 'React', icon: devIcons.react },
+    vue: { name: 'vue', title: 'Vue', icon: devIcons.vue },
+    // node: { name: 'node', title: 'Node.JS', icon: devIcons.nodejs },
+    // express: { name: 'express', title: 'Express', icon: devIcons.express },
+    mongodb: { name: 'mongodb', title: 'MongoDB', icon: devIcons.mongodb },
+    jest: { name: 'jest', title: 'Jest', icon: devIcons.jest },
+    webpack: { name: 'webpack', title: 'Webpack', icon: devIcons.webpack },
+    npm: { name: 'npm', title: 'NPM', icon: devIcons.npm },
+    django: { name: 'django', title: 'Django', icon: devIcons.django },
+    numpy: { name: 'numpy', title: 'Numpy', icon: devIcons.numpy },
+    pandas: { name: 'pandas', title: 'Pandas', icon: devIcons.pandas },
+};
 
-export default devItems
+export default devItems;
