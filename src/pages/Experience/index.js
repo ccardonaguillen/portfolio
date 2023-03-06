a3d6d1f35c98add5fe3a81dca581fb5cce0aead2
ccardona <ccardonaguillen@gmail.com>
2023-03-06 23:20:18 +0100

    Refactor Experience page to separate sections into components

--- src/pages/Experience/index.js
+++ src/pages/Experience/index.js
@@ -1,35 +1,39 @@
+import { Fragment } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBook, faDownload, faGears, faSchool } from '@fortawesome/free-solid-svg-icons';
 
 import EducationCard from '../../components/Card/EducationCard';
 import ExperienceCard from '../../components/Card/ExperienceCard';
-
 import workExperience from '../../assets/lists/workExperience';
 import education from '../../assets/lists/education';
 
 import './style.css';
+import HeroSection from '../../components/HeroSection';
+
+function CallToAction() {
+    return (
+        <Fragment>
+            <button id="download-resume">
+                <span>Download full resumé</span>
+                <FontAwesomeIcon icon={faDownload} />
+            </button>
+            {/* <button id="go-to-edcuation">
+                <FontAwesomeIcon icon={faSchool} />
+
+                <span>My education</span>
+            </button> */}
+        </Fragment>
+    );
+}
 
 export default function Experience() {
     return (
         <main id="experience">
-            <section className="hero">
-                <h1 className="hero-heading">"Not all those who wander are lost"</h1>
-                <p className="hero-subheading">
-                    Keep reading to check where my journey has taken me through a short but windy
-                    path
-                </p>
-                <div className="call-to-action">
-                    {/* <button id="go-to-edcuation">
-                        <FontAwesomeIcon icon={faSchool} />
-
-                        <span>My education</span>
-                    </button> */}
-                    <button id="download-resume">
-                        <span>Download full resumé</span>
-                        <FontAwesomeIcon icon={faDownload} />
-                    </button>
-                </div>
-            </section>
+            <HeroSection
+                heading='"Not all those who wander are lost"'
+                subheading="Keep reading to check where my journey has taken me through a short but windy path"
+                cta={<CallToAction />}
+            />
             <section id="work">
                 {workExperience.map((details) => (
                     <ExperienceCard key={details.title} {...details} />
